1333995ee6ea4cda2a65971e5da9ad68dff26f98
mjc <1119250897@qq.com>
2019-02-01 18:15:13 +0800

    aop 在了解一下

---
+++ src/main/java/com/mjc/voice/aop/log/DemoAnnotationService.java
@@ -0,0 +1,13 @@
+package com.mjc.voice.aop.log;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by gcb on 2019/2/1.
+ */
+@Service
+public class DemoAnnotationService {
+    @Action(name = "注解式 add方法")
+    public void add(){}
+
+}
---
+++ src/main/java/com/mjc/voice/aop/log/LogAspect.java
@@ -0,0 +1,12 @@
+package com.mjc.voice.aop.log;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by gcb on 2019/2/1.
+ */
+@Aspect
+@Component
+public class LogAspect {
+}
